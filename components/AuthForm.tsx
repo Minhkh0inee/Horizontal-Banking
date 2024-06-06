2f40b148783a66edd3b1aeb0c2a1611f2fe3f7c6
MinhKhoi <mkhoi1776@gmail.com>
2024-06-06 21:39:45 +0700

    Implement auth, appwrite, sentry

---
+++ components/AuthForm.tsx
@@ -0,0 +1,216 @@
+"use client";
+
+import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import React, { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { authFormSchema } from "@/lib/utils";
+import CustomInput from "./CustomInput";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.action";
+
+const AuthForm = ({ type }: { type: string }) => {
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formSchema = authFormSchema(type);
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(data: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+
+    try {
+      // Sign up with Appwrite
+
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
+      }
+
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8">
+        <Link href="/" className="cursor-pointer flex items-center gap-1 px-4">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="horizontal logo"
+          />
+
+          <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+            Horizontal
+          </h1>
+        </Link>
+        <div className="flex flex-col gap-1 md:gap-3">
+          <h1 className="text-24 lg:text-36 font-semibold text-gray-900">
+            {user ? "Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}
+          </h1>
+
+          <p className="text-16 font-normal text-gray-600">
+            {user
+              ? "Link your account to get started"
+              : "Please enter your details"}
+          </p>
+        </div>
+      </header>
+
+      {user ? (
+        <div className="flex flex-col gap-4">{/* PlaidLink */}</div>
+      ) : (
+        <>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              {type === "sign-up" && (
+                <>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="firstName"
+                      placeholder="Enter your first name"
+                      label="First Name"
+                    />
+
+                    <CustomInput
+                      control={form.control}
+                      name="lastName"
+                      placeholder="Enter your last name"
+                      label="Last Name"
+                    />
+                  </div>
+                  <CustomInput
+                    control={form.control}
+                    name="address1"
+                    placeholder="Enter your address"
+                    label="Address"
+                  />
+
+                  <CustomInput
+                    control={form.control}
+                    name="city"
+                    placeholder="Enter your city"
+                    label="City"
+                  />
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      placeholder="Example: NY"
+                      label="State"
+                    />
+
+                    <CustomInput
+                      control={form.control}
+                      name="postalCode"
+                      placeholder="Example: 11101"
+                      label="Postal Code"
+                    />
+                  </div>
+
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="dateOfBirth"
+                      placeholder="YYYY-MM-DD"
+                      label="Date of Birth"
+                    />
+
+                    <CustomInput
+                      control={form.control}
+                      name="ssn"
+                      placeholder="Example: 1234"
+                      label="SSN"
+                    />
+                  </div>
+                </>
+              )}
+
+              <CustomInput
+                control={form.control}
+                name="email"
+                placeholder="Enter your email"
+                label="Email"
+              />
+
+              <CustomInput
+                control={form.control}
+                name="password"
+                placeholder="Enter your password"
+                label="Password"
+              />
+
+              <div className="flex flex-col gap-4">
+                <Button type="submit" disabled={isLoading} className="form-btn">
+                  {isLoading ? (
+                    <>
+                      <Loader2 className="animate-spin" size={20} /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+
+          <footer className="flex justify-center gap-1">
+            <p className="text-14 font-normal text-gray-600">
+              {type === "sign-in"
+                ? "Don't have an account?"
+                : "Already have an account"}
+            </p>
+            <Link
+              className="form-link"
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
+            </Link>
+          </footer>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default AuthForm;
---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://383874f8eed52c4e1d7a28c627689da2@o4506950398050304.ingest.us.sentry.io/4507385449873408",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
